9c97fc37d7ad141a92b6c3bed64a8029c9cbee0b
Mykola Stotskyi <stotskyi.m@gmail.com>
2021-08-30 01:48:07 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,29 +1,58 @@
 import './sass/main.scss';
+import '../node_modules/basicLightbox/dist/basicLightbox.min.css'
 import photoCardTpl from './templates/photocardtpl.hbs'
-import PicsApiService from './apiService.js'
+import PicsApiService from './js/apiService.js'
+import OnTopBtn from './js/ontopbtn.js'
+import '@pnotify/core/dist/BrightTheme.css';
+import "@pnotify/core/dist/PNotify.css";
+import { error, notice} from "@pnotify/core";
+
+
 
 
 const refs = {
     search: document.querySelector('#search-form'),
     gallery: document.querySelector('.gallery'),
-    loadMore: document.querySelector('.load-more-btn')
+    loadMore: document.querySelector('.load-more-btn'),
+    modal: document.querySelector('.lightbox'),
+    modalCloseBtn: document.querySelector('button[data-action="close-lightbox"]'),
+    overlayClickModalClose: document.querySelector('.lightbox__overlay'),
+    modalIsOpenImg: document.querySelector('.lightbox__image'),
+ 
 }
+
+const onTopBtn = new OnTopBtn ();
+
+onTopBtn.onTop();
+
 
 const picsApiService = new PicsApiService ();
 
 
 function onSearch (e) {
     e.preventDefault();
+  picsApiService.query = e.currentTarget.elements.query.value.trim();
+if (picsApiService.query ==='') {
+   error({
+    title: 'Введите свой запрос',
+    text: ''
+  }); 
+  return
+}
+
     refs.gallery.innerHTML = '';
-   picsApiService.query = e.currentTarget.elements.query.value.trim();
+ console.log(picsApiService.hits);
    picsApiService.resetPage()
-   picsApiService.fetchPictures().then(pics=>{renderCard (pics)})
-   renderCard ()
+   picsApiService.fetchPictures().then(pics=>{renderCard (pics), notice ({
+    title: `Найдено ${picsApiService.hits} совпадений`,
+    text: ''
+  })})
+  //  renderCard ()
   }
 
 function onLoadMore () {
     picsApiService.fetchPictures().then(pics=>{renderCard (pics)})
-    renderCard ()
+    // renderCard ()
 }
 
 function renderCard (pics){
@@ -31,5 +60,45 @@ function renderCard (pics){
     refs.gallery.insertAdjacentHTML('beforeend', picsCard)
   }
 
-refs.search.addEventListener('submit', onSearch)
-refs.loadMore.addEventListener('click', onLoadMore)
+refs.search.addEventListener('submit', onSearch);
+refs.loadMore.addEventListener('click', onLoadMore);
+refs.gallery.addEventListener('click', modalOpen);
+
+function modalOpen (event){
+
+  event.preventDefault();
+  console.log(event);
+  if (event.target.nodeName !== "IMG"){
+    return;
+  }
+ modalIsOpen(event);
+};
+
+
+
+function modalIsOpen(event) {
+  modalToggle();
+  refs.modalIsOpenImg.src = event.target.dataset.source;
+  refs.modalIsOpenImg.alt = event.target.alt;
+}
+
+function modalToggle(){
+refs.modal.classList.toggle('is-open')
+};
+
+refs.overlayClickModalClose.addEventListener('click', modalIsClose);
+refs.modalCloseBtn.addEventListener("click", modalIsClose);
+window.addEventListener('keyup', closeModalOnESC);
+
+function modalIsClose (){
+  modalToggle();
+  refs.modalIsOpenImg.src = '';
+}
+
+function closeModalOnESC(event) {
+  if (event.key !== 'Escape'){
+    return;
+  };
+  modalIsClose ();
+};
+
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -4,19 +4,20 @@ export default class PicsApiService{
 constructor() {
 this.searchQuery ='';
 this.page = 1;
+this.hits = 0;
 
 }
 
 fetchPictures () {
     const API_KEY = '23134043-5454bb2c28435773c6d2778ec';
     const BASE_URL = 'https://pixabay.com/api/';
-    let page = 1;
     const URL = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
     return fetch(URL)
         .then(response => response.json())
         .then(data=>{
         this.incrementPage();
-            return data.hits})
+        this.getHits (data.totalHits);
+        return data.hits})
 }
 
 incrementPage(){
@@ -26,6 +27,12 @@ incrementPage(){
 resetPage() {
     this.page = 1;
     }
+
+getHits (totalHits) {
+
+    this.hits = totalHits
+
+}
 
 get query() {
     return this.searchQuery;
