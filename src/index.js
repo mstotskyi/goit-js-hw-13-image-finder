5b6dba925c0bc5854413df04a25d012ac3b3c868
Mykola Stotskyi <stotskyi.m@gmail.com>
2021-09-03 15:05:45 +0300

    fix index.json

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import PicsApiService from './js/apiService.js'
 import OnTopBtn from './js/ontopbtn.js'
 import '@pnotify/core/dist/BrightTheme.css';
 import "@pnotify/core/dist/PNotify.css";
-import { error, notice, alert} from "@pnotify/core/dist/PNotify";
+import { error, notice} from "@pnotify/core";
 
 const refs = {
     search: document.querySelector('#search-form'),
@@ -16,14 +16,10 @@ const refs = {
     modalIsOpenImg: document.querySelector('.lightbox__image'),
     spinner: document.querySelector('.spinner'),
  }
-
 const onTopBtn = new OnTopBtn ();
-
 onTopBtn.onTop();
 
 const picsApiService = new PicsApiService ();
-
-
 
 function onSearch (e) {
     e.preventDefault();
@@ -34,99 +30,85 @@ function onSearch (e) {
     text: 'Поле запроса пустое!'
   }); 
   return
-}
-   picsApiService.fetchPictures().then(pics=>{
-      if (picsApiService.hits === 0){
-        alert ({
-          title: `Внимание!`,
-          text: `Некорректный запрос`
-        })   
-        return
-      } 
-      else {
-        loadMoreBtnShow ()
+} 
+
+    
     refs.gallery.innerHTML = '';
-    loadMoreBtnDisabled ();
     picsApiService.resetPage();
-        renderCard (pics),
-        notice ({
-          title: `Поздравляем!`,
-          text: `Найдено ${picsApiService.hits} совпадений!`}),
-        loadMoreBtnEnable (), scrollIntoView()
-    };
+       
+    picsApiService.fetchPictures().then(pics=>{
+      if (picsApiService.hits === 0) {
+        
+   notice ({
+    title: `Внимание!`,
+    text: ``
   })
+  return
+      }
+      loadMoreBtnShow ()
+      loadMoreBtnDisabled (); 
+      renderCard (pics), notice ({
+    title: `Поздравляем!`,
+    text: `Найдено ${picsApiService.hits} совпадений!`
+  })
+loadMoreBtnEnable ();
+})
+      
  }
 
 function onLoadMore () {
-    loadMoreBtnDisabled ();
-    picsApiService.fetchPictures().then(pics=>{renderCard (pics),
-      loadMoreBtnEnable ()
-      scrollIntoView()
-   }) 
-}
+      loadMoreBtnDisabled ();
+      picsApiService.fetchPictures().then(pics=>{renderCard (pics), scrollIntoView(pics[0].id)
+     })
 
-function scrollIntoView () {
-     const element = document.getElementById('body');
-      element.scrollIntoView({
-        behavior: 'smooth',
-        block: "end",
-      });
+     loadMoreBtnEnable ();
 }
 
 function renderCard (pics){
     const picsCard = photoCardTpl (pics);
     refs.gallery.insertAdjacentHTML('beforeend', picsCard)
+    scrollIntoView ()
 }
-
 refs.search.addEventListener('submit', onSearch);
 refs.loadMore.addEventListener('click', onLoadMore);
 refs.gallery.addEventListener('click', modalOpen);
-
 function modalOpen (event){
 
   event.preventDefault();
+  console.log(event);
   if (event.target.nodeName !== "IMG"){
     return;
   }
  modalIsOpen(event);
 };
-
 function modalIsOpen(event) {
   modalToggle();
   refs.modalIsOpenImg.src = event.target.dataset.source;
   refs.modalIsOpenImg.alt = event.target.alt;
 }
-
 function modalToggle(){
 refs.modal.classList.toggle('is-open')
 };
-
 refs.overlayClickModalClose.addEventListener('click', modalIsClose);
 refs.modalCloseBtn.addEventListener("click", modalIsClose);
 window.addEventListener('keyup', closeModalOnESC);
-
 function modalIsClose (){
   modalToggle();
   refs.modalIsOpenImg.src = '';
 }
-
 function closeModalOnESC(event) {
   if (event.key !== 'Escape'){
     return;
   };
   modalIsClose ();
 };
-
 function loadMoreBtnShow () {
   refs.loadMore.classList.remove('is-hidden');
 }
-
 function loadMoreBtnEnable () {
   refs.loadMore.classList.remove('is-hidden');
   refs.loadMore.disabled = false;
   refs.spinner.classList.add('is-hidden');
-  
- 
 };
 
 function loadMoreBtnDisabled () { 
@@ -135,3 +117,10 @@ function loadMoreBtnDisabled () {
   refs.spinner.classList.remove('is-hidden');
 }
 
+function scrollIntoView () {
+  const element = document.getElementById('body');
+      element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+    });
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -10,10 +10,12 @@ fetchPictures () {
     const API_KEY = '23134043-5454bb2c28435773c6d2778ec';
     const BASE_URL = 'https://pixabay.com/api/';
     const URL = `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+    console.log(this.page);
     return fetch(URL)
         .then(response => response.json())
         .then(data=>{
         this.incrementPage();
+        console.log(this.page);
         this.getHits (data.totalHits);
         return data.hits})
 }
